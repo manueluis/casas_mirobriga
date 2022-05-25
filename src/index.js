86b35e12518878c83a425adee0dcef622ddd41fb
unknown <2191226@my.ipleiria.pt>
2022-05-25 09:08:17 +0100

    Codigo compactado em funções

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import { Sky } from 'three/examples/jsm/objects/Sky.js';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { Vector2 } from 'three';
 
-let camera, scene, renderer, controls, stats, fireLight, skyGeo;
+let camera, scene, renderer, controls, fireLight, skyGeo;
 
 const objects = [];
 
@@ -16,11 +17,17 @@ let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
 let canJump = false;
+let starsInited = false;
+let cloudsInited = false;
 
-let smokeMaterial, smokeParticles = [];
+let intersetado;
+
+let cloudMaterial, clouds = [];
+let objectsHotspot = [];
+let spheres = [];
 
 const d = new Date();
-let hour = 18;
+let hour = d.getHours();
 let sky, sun;
 
 const cameraFloorDistance = 1.3;
@@ -42,10 +49,6 @@ Nebula.fromJSONAsync(json, THREE).then(loaded => {
 });
 
 function init() {
-
-    stats = new Stats()
-    document.body.appendChild(stats.domElement)
-
     camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.01, 6000 + hour * 5);
     camera.position.y = cameraFloorDistance;
     camera.position.x = 0;
@@ -54,98 +57,16 @@ function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x001844);
 
-    sky = new Sky();
-    sky.scale.setScalar(450000);
-    scene.add(sky);
-
-    sun = new THREE.Vector3();
-
-    const effectController = {
-        turbidity: 10,
-        rayleigh: 0.75,
-        mieCoefficient: 0.005,
-        mieDirectionalG: 0.7,
-        elevation: (hour > 18) ? 315 - hour*15 : -90 + hour*15,
-        azimuth: 180,
-        hourEC:hour
-    };
-
-    console.log(effectController.elevation);
-
-    const uniforms = sky.material.uniforms;
-    uniforms['turbidity'].value = effectController.turbidity;
-    uniforms['rayleigh'].value = effectController.rayleigh;
-    uniforms['mieCoefficient'].value = effectController.mieCoefficient;
-    uniforms['mieDirectionalG'].value = effectController.mieDirectionalG;
-
-    function guiChanged() {
-        hour = effectController.hourEC;
-        effectController.elevation = (hour > 12) ? 90 + (hour-12)*11.25 : -90 + hour*15,
-        uniforms[ 'sunPosition' ].value.copy( sun );
-
-        const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
-        const theta = THREE.MathUtils.degToRad(effectController.azimuth);
-
-        sun.setFromSphericalCoords(1, phi, theta);
-        uniforms['sunPosition'].value.copy(sun);
-    }
-
-    const gui = new GUI();
-    gui.add( effectController, 'hourEC', 0, 24, 1 ).onChange( guiChanged );
-    guiChanged();
-
-    const hemisphereLight = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
-    hemisphereLight.position.set(0.5, 1, 0.75);
-    scene.add(hemisphereLight);
-
-    const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
-    scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(0, 1000, 0);
-    directionalLight.target = camera;
-    directionalLight.position.multiplyScalar(30);
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
-    directionalLight.shadow.mapSize.width = 2048;
-    directionalLight.shadow.mapSize.height = 2048;
-    directionalLight.intensity = (hour > 4 && hour < 20) ? 1 : 0;
-    const d = 50;
-    directionalLight.shadow.camera.left = - d;
-    directionalLight.shadow.camera.right = d;
-    directionalLight.shadow.camera.top = d;
-    directionalLight.shadow.camera.bottom = - d;
-    directionalLight.shadow.camera.far = 3500;
-    directionalLight.shadow.bias = - 0.0001;
-
-    const dirLightHelper = new THREE.DirectionalLightHelper(directionalLight, 10);
-    scene.add(dirLightHelper);
-
-    const directionalLightMoon = new THREE.DirectionalLight(0x506886, 1);
-    directionalLightMoon.position.set(0, (hour > 4 && hour < 20) ? -1 : 1, (hour > 12) ? 12 - hour : 12 - (hour / 2));
-    directionalLightMoon.position.multiplyScalar(30);
-    directionalLightMoon.castShadow = true;
-    scene.add(directionalLightMoon);
-    directionalLightMoon.shadow.mapSize.width = 2048;
-    directionalLightMoon.shadow.mapSize.height = 2048;
-    directionalLightMoon.intensity = (hour > 4 && hour < 20) ? 0 : 1;
-    directionalLightMoon.shadow.camera.left = - d;
-    directionalLightMoon.shadow.camera.right = d;
-    directionalLightMoon.shadow.camera.top = d;
-    directionalLightMoon.shadow.camera.bottom = - d;
-    directionalLightMoon.shadow.camera.far = 3500;
-    directionalLightMoon.shadow.bias = - 0.0001;
-
-    const dirLightHelperMoon = new THREE.DirectionalLightHelper(directionalLightMoon, 10);
-    scene.add(dirLightHelperMoon);
-
-    fireLight = new THREE.PointLight(0xFFB900, 1, 100);
-    scene.add(fireLight);
+    initSky();
+    initLights();
+    initLoaders();
 
     controls = new PointerLockControls(camera, document.body);
 
     const blocker = document.getElementById('blocker');
     const instructions = document.getElementById('instructions');
+
+    window.addEventListener('resize', onWindowResize);
 
     instructions.addEventListener('click', function () {
         controls.lock();
@@ -194,6 +115,9 @@ function init() {
                 canJump = false;
                 break;
 
+            case 'KeyF':
+                hotspotTest();
+                break;
         }
         if (event.shiftKey) velocityScalar = 80;
 
@@ -231,6 +155,84 @@ function init() {
     document.addEventListener('keydown', onKeyDown);
     document.addEventListener('keyup', onKeyUp);
 
+    generateHotSpot(0, -5, 20, -Math.PI / 2, '../images/teste.png')
+}
+
+//Ceu
+function initSky() {
+    sky = new Sky();
+    sky.scale.setScalar(450000);
+    scene.add(sky);
+
+    sun = new THREE.Vector3();
+
+    const effectController = {
+        turbidity: 0.4,
+        rayleigh: 0.75,
+        mieCoefficient: 0.005,
+        mieDirectionalG: 0.85,
+        elevation: (hour > 18) ? 315 - hour*15 : -90 + hour*15,
+        azimuth: 180,
+        hourEC:hour
+    };
+
+    console.log(effectController.elevation);
+
+    const uniforms = sky.material.uniforms;
+    uniforms['turbidity'].value = effectController.turbidity;
+    uniforms['rayleigh'].value = effectController.rayleigh;
+    uniforms['mieCoefficient'].value = effectController.mieCoefficient;
+    uniforms['mieDirectionalG'].value = effectController.mieDirectionalG;
+
+    function guiChanged() {
+        hour = effectController.hourEC;
+        effectController.elevation = (hour > 12) ? 90 + (hour-12)*11.25 : -90 + hour*15,
+        uniforms[ 'sunPosition' ].value.copy( sun );
+
+        const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+        const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+
+        sun.setFromSphericalCoords(1, phi, theta);
+        uniforms['sunPosition'].value.copy(sun);
+        changeStars();
+        changeClouds();
+    }
+
+    const gui = new GUI();
+    gui.add( effectController, 'hourEC', 0, 24, 1 ).onChange( guiChanged );
+    guiChanged();
+}
+
+//Luzes
+function initLights() {
+    const hemisphereLight = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
+    hemisphereLight.position.set(0.5, 1, 0.75);
+    scene.add(hemisphereLight);
+
+    const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(0, 1000, 0);
+    directionalLight.target = camera;
+    directionalLight.position.multiplyScalar(30);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+    const d = 50;
+    directionalLight.shadow.camera.left = - d;
+    directionalLight.shadow.camera.right = d;
+    directionalLight.shadow.camera.top = d;
+    directionalLight.shadow.camera.bottom = - d;
+    directionalLight.shadow.camera.far = 3500;
+    directionalLight.shadow.bias = - 0.0001;
+
+    fireLight = new THREE.PointLight(0xFFB900, 1, 100);
+    scene.add(fireLight);
+}
+
+function initLoaders() {
     var carregador = new GLTFLoader()
     carregador.load(
         modelos,
@@ -250,10 +252,11 @@ function init() {
     renderer.shadowMap.enabled = true;
 
     document.body.appendChild(renderer.domElement);
+}
 
-    window.addEventListener('resize', onWindowResize);
-
-    if (hour <= 21 && hour >= 6) {
+function changeClouds(){
+    if ((hour <= 21 && hour >= 3) && !cloudsInited) {
+        cloudsInited = true;
         const loader = new THREE.TextureLoader();
 
         loader.crossOrigin = '';
@@ -261,37 +264,45 @@ function init() {
         loader.load(
             '../images/cloud.png',
             function onLoad(texture) {
-                const smokeGeo = new THREE.CircleBufferGeometry((Math.random() * 600) + 450, (Math.random() * 600) + 450);
+                const cloudGeo = new THREE.CircleBufferGeometry((Math.random() * 600) + 450, (Math.random() * 600) + 450);
 
-                smokeMaterial = new THREE.MeshBasicMaterial({
+                cloudMaterial = new THREE.MeshBasicMaterial({
                     map: texture,
                     transparent: true,
                     depthWrite: false,
-                    opacity: 0.8
+                    opacity: (hour<12)?hour/12:24/hour
                 });
 
                 for (let p = 0, l = 50; p < l; p++) {
-                    let particle = new THREE.Mesh(smokeGeo, smokeMaterial);
+                    let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
 
-                    particle.rotateX(Math.PI / 2);
+                    cloud.rotateX(Math.PI / 2);
 
-                    particle.position.set(
+                    cloud.position.set(
                         (Math.random() * 6000) - 3000,
                         400 + Math.random() * 200,
                         (Math.random() * 6000) - 3000
                     );
 
-                    particle.rotation.z = Math.random() * 360;
-                    scene.add(particle);
-                    smokeParticles.push(particle);
+                    cloud.rotation.z = Math.random() * 360;
+                    scene.add(cloud);
+                    clouds.push(cloud);
                 }
             }
         );
-    }
+    } else if (hour > 21 || hour < 3) {
+        clouds.forEach(cloud => scene.remove(cloud));
+        clouds = [];
+        cloudsInited = false;
+    } else {
+        clouds.forEach(cloud => cloud.material.opacity = (-Math.pow(hour-13, 2)/49 + 1));
+    }  
+}
 
-    if (hour > 21 || hour < 6) {
-        let spheres = [];
-        for (let i = 1; i < 300; i++) {
+function changeStars(){
+    if ((hour > 19 || hour < 8) && !starsInited) {
+        starsInited = true;
+        for (let i = 1; i < 800; i++) {
             let geometry = new THREE.SphereGeometry(2 + Math.random() * 3, 6, 6);
             let material = new THREE.MeshBasicMaterial({
                 color: new THREE.Color(255, 205, 60)
@@ -302,8 +313,41 @@ function init() {
             spheres.push(sphere);
             sphere.position.setFromSpherical(new THREE.Spherical(3000 + 5 * Math.random(), 2 * Math.PI * Math.random(), 2 * Math.PI * Math.random()))
         }
+    } else if (hour >= 8 && hour <= 19) {
+        spheres.forEach(star => scene.remove(star));
+        spheres = [];
+        starsInited = false;
     }
 }
+
+//Hotspots
+function generateHotSpot(positionx, positiony, positionz, rotation, image){
+    var texture = new THREE.TextureLoader().load(image);
+    var material = new THREE.MeshBasicMaterial({
+      map: texture
+    });
+    var geometry = new THREE.BoxGeometry(0, 5, 10);                                                             
+    var mesh = new THREE.Mesh(geometry, material);
+    var s = 0.5;
+    mesh.scale.set(s, s, s);
+    mesh.position.set(positionx, positiony, positionz); //mesh.receiveShadow = true;
+
+    scene.add(mesh);
+    objectsHotspot.push(mesh); //Chao
+
+    positionx += 0.1;
+
+    texture = new THREE.TextureLoader().load('../images/info.png');
+    material = new THREE.MeshBasicMaterial({
+      map: texture
+    });
+    geometry = new THREE.CircleGeometry(0.8, 32);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(positionx, positiony, positionz);
+    mesh.rotation.y = rotation;
+    scene.add(mesh); //i chao
+}
+
 
 function onWindowResize() {
 
@@ -312,6 +356,35 @@ function onWindowResize() {
 
     renderer.setSize(window.innerWidth, window.innerHeight);
 
+}
+
+//Mostrar
+function hotspotTest() {
+    console.log("test")
+    var raycasterHotspot = new THREE.Raycaster();
+    raycasterHotspot.setFromCamera(new Vector2(0,0),camera);
+
+    const hotspotIntersection = raycasterHotspot.intersectObjects(objectsHotspot);
+    console.log(raycasterHotspot)
+    if(hotspotIntersection.length > 0) {
+        console.log(hotspotIntersection[0])
+        if(hotspotIntersection[0].distance <= 10){
+            console.log("a mostrar")
+            intersetado = hotspotIntersection[0].object;
+            intersetado.visible = true;
+        } else {
+            if (intersetado != null) {
+                intersetado.visible = false;
+                intersetado = null;
+            }
+        }
+    } else {
+        if (intersetado != null) {
+            console.log("lol")
+            intersetado.visible = false;
+            intersetado = null;
+        }
+    }
 }
 
 function cameraColision(raycaster, axis) {
@@ -338,6 +411,12 @@ function animate(nebula, app) {
     nebula.update();
 
     const time = performance.now();
+
+    objectsHotspot.forEach(object => {
+        if(object.visible && controls.getObject().position.distanceTo(object.position)>10){
+            object.visible = false;
+        }
+    });
 
     if (controls.isLocked === true) {
 
@@ -432,8 +511,6 @@ function animate(nebula, app) {
     prevTime = time;
 
     nebula.update();
-
-    stats.update();
 
     renderer.render(scene, camera);
 
